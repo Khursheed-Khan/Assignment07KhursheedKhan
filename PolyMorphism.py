4c2a1218ea61d55e2bb545633bc4e4214cd7620c
Khursheed-Khan <64682216+Khursheed-Khan@users.noreply.github.com>
2024-11-26 13:52:13 -0800

    Updated on Nov 26 at 1pm Pacific with exception handling.

--- PolyMorphism.py
+++ PolyMorphism.py
@@ -1,3 +1,9 @@
+class FirstNameException(ValueError):
+    pass
+
+class LastNameException(ValueError):
+    pass
+
 class Person:
 
     __first_name: str
@@ -14,7 +20,8 @@ class Person:
     @first_name.setter
     def first_name(self, value):
         if (not value.isalpha()):
-            raise ValueError("First Name must be alphanumeric")
+            #raise ValueError("First Name must be alphanumeric")
+            raise FirstNameException("First Name must be alphanumeric")
         self.__first_name = value
 
     @property
@@ -24,7 +31,8 @@ class Person:
     @last_name.setter
     def last_name(self, value):
         if (not value.isalpha()):
-            raise ValueError("Last Name must be alphanumeric")
+            #raise ValueError("Last Name must be alphanumeric")
+            raise LastNameException("Last Name must be alphanumeric")
         self.__last_name = value
 
     def print_intro(self):
@@ -52,9 +60,9 @@ class Person:
 
 class Student(Person):
     __gpa: float
-    def __init__(self, user_first_name, user_last_name,user_gpa): # Overloading __init__
-        super().__init__(user_first_name, user_last_name)
-        self.gpa = user_gpa
+    def __init__(self, user_first_name:str, user_last_name:str,user_gpa:float): # Overloading __init__
+        super().__init__(user_first_name, user_last_name) # Initializing parent class
+        self.gpa = user_gpa # Initializing Student_Class Attrributes --> setting up consturctor, creating instance
 
     @property
     def gpa(self):
@@ -69,39 +77,54 @@ class Student(Person):
     def __str__(self):
         return f"{super().__str__()},GPA: {self.gpa}"
 
+    @staticmethod
+    def from_json(data):
+        return Student(data['first_name'], data['last_name'], data['gpa'])
+
 
 s = Student(user_first_name='Vic', user_last_name='Smith', user_gpa=3.0)
-print("="*50)
+print("s"*50)
 print(s)
 print("="*50)
 
 p = Person (user_first_name='Khursheed', user_last_name='Smith')
-print("*"*50)
+print("p"*50)
 print(p)
-print("*"*50)
+print("p"*50)
+
+s2 = Student.from_json({"first_name":"Khursheed", "last_name":"Khan", "gpa":3.0})
+print("S2"*50)
+print(s2)
+print("s2"*50)
+
+s3 = Student('Lolla', 'Volla', 4.0)
+print("S3"*50)
+print(s3)
+print("S3"*50)
+
 
 # More comments
 
 class IO:
     @staticmethod
     def input_data_to_table(student_data: list[Student]):
-        try:
+        #try:
             student_first_name = input("First name: ")
-            if not student_first_name.isalpha():
-                raise ValueError ("First name must be alphanumeric")
+            #if not student_first_name.isalpha():
+            #   raise ValueError ("First name must be alphanumeric")
             student_last_name = input("Last name: ")
-            if not student_last_name.isalpha():
-                raise ValueError ("Last name must be alphanumeric")
+            #if not student_last_name.isalpha():
+            #    raise ValueError ("Last name must be alphanumeric")
 
             call = Student(student_first_name, student_last_name, user_gpa=3.0)
             student_data.append(call)
 
-        except ValueError as e:
-            print("Only use names without numbers ",e)
+        #except ValueError as e:
+            #print("Only use names without numbers ",e)
 
-        return student_data
+            return student_data
 
-Student.print_intro_static()
+#Student.print_intro_static()
 
 roster = []
 while True:
